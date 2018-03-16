22c917990e35fe1e7aeb1acac8ced3d6caa8f50f
yashvitmaggi <yash.maggirajput@gmail.com>
2018-03-16 11:07:19 +0530

    Create code.y
    Using simple neural network for cat recognition. Got the accuracy of 99% on train set and 70% on test set but this can be improved with the increase in number of iterations and learning rate. Next We will perform complex neural network with hidden layers.

---
+++ code.py
@@ -0,0 +1,149 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+
+%matplotlib inline
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1).T
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+# GRADED FUNCTION: sigmoid
+
+def sigmoid(z):
+  
+    s = 1/(1+np.exp(-z))
+    
+    return s
+# GRADED FUNCTION: initialize_with_zeros
+
+def initialize_with_zeros(dim):
+
+    w = np.zeros((dim,1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b    
+# GRADED FUNCTION: propagate
+
+def propagate(w, b, X, Y):
+
+    m = X.shape[1]
+  
+    A = 1/(1+np.exp(-(np.dot(w.T,X)+b)))                                  # compute activation
+    cost = np.sum(np.multiply(Y,np.log(A))+np.multiply((1-Y),np.log(1-A)),axis = 1, keepdims = True)/-m                                 # compute cost
+    
+    dw = np.dot(X,(A-Y).T)/m
+    db = np.sum(A-Y,axis = 1, keepdims = True)/m
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+ 
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+# GRADED FUNCTION: optimize
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    
+    costs = []
+    m = X.shape[1]
+    
+    for i in range(num_iterations):
+        
+        grads, cost = propagate(w, b, X, Y)   
+        
+        dw = grads["dw"]
+        db = grads["db"]
+        
+
+        w = w-learning_rate*dw
+        b = b-learning_rate*db
+       
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+    
+# GRADED FUNCTION: predict
+
+def predict(w, b, X):
+    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+   
+    A = sigmoid(np.dot(w.T,X) + b)    
+    for i in range(A.shape[1]):
+
+        pass
+        if float(A[0,i])>0.5:
+            Y_prediction[0,i]=1
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction  
+    
+    # GRADED FUNCTION: model
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+
+    m = X_train.shape[0]
+  
+    
+    # initialize parameters with zeros
+    w, b = initialize_with_zeros(m)
+
+    # Gradient descent
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    # Predict test/train set examples 
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+    return d
+    
